ea2ee5ebe77085f97bb29b6f8dfb00c81d11eb42
Rojen Chakradhar <chakradhar@rojen.me>
2025-04-21 00:02:52 +0545

    about page created and few changes made

---
+++ scripts/injectComponents.js
@@ -0,0 +1,11 @@
+fetch("../components/navbar.html")
+	.then((res) => res.text())
+	.then((data) => {
+		document.getElementById("navbar-placeholder").innerHTML = data;
+	});
+
+fetch("../components/footer.html")
+	.then((res) => res.text())
+	.then((data) => {
+		document.getElementById("footer-placeholder").innerHTML = data;
+	});
