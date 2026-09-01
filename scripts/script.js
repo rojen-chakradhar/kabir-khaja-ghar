789dad82c3dd630d4d7c492c4f58c7e53a27dda1
Rojen Chakradhar <chakradhar@rojen.me>
2026-09-02 02:59:33 +0545

    working on loader

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// review slider
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
@@ -7,24 +8,54 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-
-gsap.from('.loader-name', {
-  opacity: 0,
-  y: 100,
-  duration:1,
-})
-
-gsap.from('.est-date', {
-  opacity: 0,
-  y: 100,
-  delay: 0.5,
-  duration:1,
-})
-
-const loader = document.querySelector("#loader");
-setTimeout(() => {
-  loader.style.marginTop = "-100dvh";
-  setTimeout(() => {
-    loader.style.display = "none";
-  }, 2500)
-}, 2000);
+function loader() {
+  const loader = document.querySelector("#loader");
+  const bodyContainer = document.querySelector("body");
+  // const loaderNameFullForm = document.querySelectorAll(".loader-name-span");
+  // not letting users scroll when loader is showing up
+  bodyContainer.style.height= "100dvh";
+  bodyContainer.style.overflow= "hidden";
+  let tl = gsap.timeline();
+  // name and establish date showing up
+  tl.from('.loader-name', {
+    opacity: 0,
+    y: 100,
+    duration:1,
+    ease: "power2.out"
+  })
+  .from('.est-date', {
+    opacity: 0,
+    y: 50,
+    duration: 0.6,
+    ease: "power2.out"
+  })
+  .to(".loader-name", {
+    gap: "5rem",
+    duration: 2.5,
+  })
+  .to(".loader-name-spa", {
+    clipPath: "inset(0 0% 0 0)",
+    duration: 1,
+    ease: "power2.out"
+  })
+  // .from(loaderNameFullForm, {
+  //   display: "none",
+  //   opacity: 0,
+  //   delay: 1,
+  //   duration: 2,
+  //   ease: "power1.in"
+  // })
+  // loader disappear
+  .to(loader, {
+    y: -100,
+    delay: 1,
+    duration: 1.2,
+    ease: "power2.inOut",
+    onComplete: () => {
+      loader.style.display= "none"
+      bodyContainer.style.height= "100%";
+      bodyContainer.style.overflow= "auto";
+    }
+  })
+}
+loader();
